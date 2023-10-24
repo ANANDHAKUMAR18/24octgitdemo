95b224c27c224a0b2d1456b3adc32a4d5c283375
Sandhiya0205 <146726602+Sandhiya0205@users.noreply.github.com>
2023-10-24 21:10:54 +0530

    Update Maths.java

--- src/test/java/gitdempo24oct/Maths.java
+++ src/test/java/gitdempo24oct/Maths.java
@@ -12,11 +12,17 @@ public class Maths {
 		
 		System.out.println("Sum of two numbers");
 		}
+
+	  public void M3(){
+		
+		System.out.println("Sum of two numbers");
+		}
      
  	public static void main(String[] args){
         Maths obj=new Maths();
 	obj.M1();
 	obj.M2();
+	obj.M3();
 
 }
 }
