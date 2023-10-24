b478f3d1093aa24d32b363ef045d0104f3404970
AK <anandhsandy.g@gmail.com>
2023-10-24 20:54:50 +0530

    M1

---
+++ src/test/java/gitdempo24oct/Maths.java
@@ -0,0 +1,17 @@
+package gitdempo24oct;
+
+import gitdempo24oct.Maths;
+
+public class Maths {
+	
+     public void M1(){
+		
+		System.out.println("Sum of two numbers");
+		}
+     
+ 	public static void main(String[] args){
+        Maths obj=new Maths();
+	obj.M1();
+
+}
+}
